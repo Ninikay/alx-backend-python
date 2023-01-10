693fb83833eb87a158aa93cd702e5ac9a9e4cc79
Ninikay <ms.nikeoni@gmail.com>
2023-01-10 22:37:57 +0000

    total runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-""" Run time for four parallel comprehensions module
-"""
+""" module doc str """
+
 
 import asyncio
+from typing import List, Iterator, Generator
+import random
+import time
 
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ Measure the total runtime of four parallel coroutines
-    """
+    """ measures time of exec of an async coroutine"""
+    start = time.perf_counter()
+    x = await asyncio.gather(async_comprehension(), async_comprehension(),
+                             async_comprehension(), async_comprehension())
+    # print(x)
+    return time.perf_counter() - start
